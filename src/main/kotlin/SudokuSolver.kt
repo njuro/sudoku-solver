dfc9c8c9da42a35d3a3ea24151e2ec2360d4586e
njuro <juraj.noge@gmail.com>
2021-09-07 16:30:36 +0200

    Working implementation

--- src/main/kotlin/SudokuSolver.kt
+++ src/main/kotlin/SudokuSolver.kt
@@ -2,37 +2,37 @@ data class SudokuField(val row: Int, val column: Int) {
     override fun toString() = "[${row}, ${column}]"
 }
 
-fun SudokuField.getBlock() = blocks.first { this in it }
+fun SudokuField.getBox() = boxes.first { this in it }
 const val X = 0 // empty field
 val values = (1..9).toSet()
 
 
-data class SudokuBlock(val rows: IntRange, val columns: IntRange)
+data class SudokuBox(val rows: IntRange, val columns: IntRange)
 
-operator fun SudokuBlock.contains(field: SudokuField) = field.row in rows && field.column in columns
-val blocks = setOf(
-    SudokuBlock(rows = (0..2), columns = (0..2)),
-    SudokuBlock(rows = (3..5), columns = (0..2)),
-    SudokuBlock(rows = (6..8), columns = (0..2)),
-    SudokuBlock(rows = (0..2), columns = (3..5)),
-    SudokuBlock(rows = (3..5), columns = (3..5)),
-    SudokuBlock(rows = (6..8), columns = (3..5)),
-    SudokuBlock(rows = (0..2), columns = (6..8)),
-    SudokuBlock(rows = (3..5), columns = (6..8)),
-    SudokuBlock(rows = (6..8), columns = (6..8)),
+operator fun SudokuBox.contains(field: SudokuField) = field.row in rows && field.column in columns
+val boxes = setOf(
+    SudokuBox(rows = (0..2), columns = (0..2)),
+    SudokuBox(rows = (3..5), columns = (0..2)),
+    SudokuBox(rows = (6..8), columns = (0..2)),
+    SudokuBox(rows = (0..2), columns = (3..5)),
+    SudokuBox(rows = (3..5), columns = (3..5)),
+    SudokuBox(rows = (6..8), columns = (3..5)),
+    SudokuBox(rows = (0..2), columns = (6..8)),
+    SudokuBox(rows = (3..5), columns = (6..8)),
+    SudokuBox(rows = (6..8), columns = (6..8)),
 )
 
 
-typealias SudokuGrid = List<List<Int>>
+typealias SudokuGrid = List<MutableList<Int>>
 
 operator fun SudokuGrid.get(field: SudokuField) = get(field.row)[field.column]
 fun SudokuGrid.getCandidates(field: SudokuField) = getCandidatesFromRow(field)
     .intersect(getCandidatesFromColumn(field))
-    .intersect(getCandidatesFromBlock(field))
+    .intersect(getCandidatesFromBox(field))
 
 fun SudokuGrid.getCandidatesFromRow(field: SudokuField) = values.minus(get(field.row))
 fun SudokuGrid.getCandidatesFromColumn(field: SudokuField) = values.minus(map { it[field.column] })
-fun SudokuGrid.getCandidatesFromBlock(field: SudokuField) = values.minus(field.getBlock().let { block ->
+fun SudokuGrid.getCandidatesFromBox(field: SudokuField) = values.minus(field.getBox().let { block ->
     flatMapIndexed { rowIndex, row ->
         row.mapIndexedNotNull { columnIndex, value ->
             value.takeIf {
@@ -45,7 +45,9 @@ fun SudokuGrid.getCandidatesFromBlock(field: SudokuField) = values.minus(field.g
     }
 })
 
-fun solve(grid: SudokuGrid): SudokuGrid {
+fun SudokuGrid.clone() = toList().map { it.toMutableList() }
+
+fun solve(grid: SudokuGrid): SudokuGrid? {
     val possibleTurns = mutableMapOf<SudokuField, Set<Int>>()
 
     for (row in 0..8) {
@@ -57,21 +59,39 @@ fun solve(grid: SudokuGrid): SudokuGrid {
         }
     }
 
-    return grid
+    if (possibleTurns.any { it.value.isEmpty() }) {
+        return null
+    }
+
+    if (possibleTurns.isEmpty()) {
+        return grid
+    }
+
+    val (field, values) = possibleTurns.minByOrNull { it.value.size }!!
+    for (value in values) {
+        val clone = grid.clone()
+        clone[field.row][field.column] = value
+        val result = solve(clone)
+        if (result != null) {
+            return result
+        }
+    }
+
+    return null
 }
 
 fun main() {
     val grid: SudokuGrid = listOf(
-        listOf(X, 2, X, X, 3, X, 7, 5, 8),
-        listOf(4, X, X, 1, X, 7, X, 6, X),
-        listOf(X, 3, X, X, 2, X, X, X, X),
-        listOf(X, 7, 2, X, X, X, X, X, 4),
-        listOf(X, X, X, X, X, X, X, X, X),
-        listOf(3, X, X, X, X, X, 5, 7, X),
-        listOf(X, X, X, X, 6, X, X, 9, X),
-        listOf(X, 8, X, 4, X, 1, X, X, 5),
-        listOf(7, 6, 5, X, 9, X, X, 2, X)
+        mutableListOf(X, 2, X, X, 3, X, 7, 5, 8),
+        mutableListOf(4, X, X, 1, X, 7, X, 6, X),
+        mutableListOf(X, 3, X, X, 2, X, X, X, X),
+        mutableListOf(X, 7, 2, X, X, X, X, X, 4),
+        mutableListOf(X, X, X, X, X, X, X, X, X),
+        mutableListOf(3, X, X, X, X, X, 5, 7, X),
+        mutableListOf(X, X, X, X, 6, X, X, 9, X),
+        mutableListOf(X, 8, X, 4, X, 1, X, X, 5),
+        mutableListOf(7, 6, 5, X, 9, X, X, 2, X)
     )
 
-    solve(grid)
+    println(solve(grid))
 }
